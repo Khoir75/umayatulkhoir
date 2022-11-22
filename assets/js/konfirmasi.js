8930ba2801ba301e35f9ea220e6e2ff26d574d82
umayatulkhoir <umayatulkhoir753@gmail.com>
2022-11-22 09:55:44 +0700

    ci3

---
+++ assets/js/konfirmasi.js
@@ -0,0 +1,54 @@
+function hapusMenu(url) {
+
+    swal.fire({
+        title: 'are you sure?',
+              text: "Yakin ingin hapus menu",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Ya, hapus!'
+      }).then((result) => {
+          if (result.value) {
+              document.location.href = url;
+          }
+      })
+}
+
+
+
+function hapusRole(url) {
+
+    swal.fire({
+        title: 'are you sure?',
+              text: "Yakin ingin hapus role",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Ya, hapus!'
+      }).then((result) => {
+          if (result.value) {
+              document.location.href = url;
+          }
+      })
+}
+
+
+
+function hapusSubmenu(url) {
+
+    swal.fire({
+        title: 'are you sure?',
+              text: "Yakin ingin hapus sub menu",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Ya, hapus!'
+      }).then((result) => {
+          if (result.value) {
+              document.location.href = url;
+          }
+      })
+}
